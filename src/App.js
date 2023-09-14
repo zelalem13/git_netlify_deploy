6b3deaf42d9eafcefa5dd1b7377675843b383f00
zelalem13 <zola05.com>
2023-09-14 17:08:04 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         deployment app in this site!
+         deployment app from git hub!
       </header>
     </div>
   );
